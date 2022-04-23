05809174c4a067794f8830534fdfbe945212fd6a
MikeVMane <michaelvaladaomartins@gmail.com>
2022-04-22 21:40:34 -0400

    -Added form to shoppingEdit to add items to list -Added code toload shopping list onto the form -Added code so user can update shopping list items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -11,6 +12,10 @@ export class ShoppingListService {
 
       getIngredients(){
           return this.ingredients.slice();
+      }
+
+      getIngredient(index: number){
+        return this.ingredients[index];
       }
 
       //adds ingredients to the array and grabs a copy of the new list
@@ -24,6 +29,11 @@ export class ShoppingListService {
         //     this.addIngredient(ingredient);
         // }
         this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
+      updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
         this.ingredientsChanged.next(this.ingredients.slice());
       }
 }
